4034fb2379d8dfaf4de066705995ad8b354ee92d
ashiku2 <ashikur1602@gmail.com>
2024-03-11 13:30:55 +0600

    lecture 2 problem solved

---
+++ FirstProblem.java
@@ -0,0 +1,9 @@
+public class FirstProblem {
+    public static void main(String args[]) {
+        int age = 9;
+        String name = "Akash";
+        double height = 5.9;
+        boolean married = false;
+
+    }
+}
