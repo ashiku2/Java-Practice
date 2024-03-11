aab2df13d70287ff9f86950b6a43ff042ceb03e4
ashiku2 <ashikur1602@gmail.com>
2024-03-11 13:17:15 +0600

    addition code

--- FirstClass.java
+++ FirstClass.java
@@ -1,8 +1,14 @@
-class FirstClass{
+
+import java.util.*;
+
+class FirstClass {
     public static void main(String args[]) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+
+        // taking input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
