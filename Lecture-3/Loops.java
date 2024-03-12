736537afcef722fb6f18d5460461ace4e4f804e4
ashiku2 <ashikur1602@gmail.com>
2024-03-12 12:01:57 +0600

    do while loop

--- Lecture-3/Loops.java
+++ Lecture-3/Loops.java
@@ -2,8 +2,22 @@
 
 public class Loops {
     public static void main(String args[]) {
-        for (int counter = 0; counter < 100; counter = counter + 1) {
-            System.out.println("Hello world");
-        }
-    }    
+        // for (int counter = 0; counter < 100; counter = counter + 1) {
+        //     System.out.println("Hello world");
+        // }
+
+        // for (int i = 0; i < 11; i++) {
+        //         System.out.println(i);
+        //     }
+        // int i = 0;
+        // while (i<11) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        } while (i < 11);
+    }
 }
